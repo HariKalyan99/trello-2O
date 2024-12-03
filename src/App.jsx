b16561f8c30d150f2840fb310d40cca46c3e3b03
Harikalyan <harikalyan99tronics@gmail.com>
2024-12-03 11:04:35 +0530

    initial commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,45 @@
+import { useEffect } from 'react';
 import './App.css'
+import { useState } from 'react';
+import axios from 'axios';
+
+let APIKey = import.meta.env.VITE_APIKEY;
+let APIToken = import.meta.env.VITE_APITOKEN;
 
 function App() {
+  const [getData, setData] = useState([]);
+  useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+    const fetchBoards = async () => {
+      try {
+        const { data } = await axios.get(
+          `https://api.trello.com/1/members/me/boards?key=${APIKey}&token=${APIToken}`,
+          {
+            signal,
+          }
+        );
+        console.log(data);
+        setData(data);
+      } catch (error) {
+        if (error.name === "AbortError") {
+          console.log("Fetch aborted");
+        } else {
+          console.warn(error);
+        }
+      }
+    };
+
+    fetchBoards();
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
   return (
-    <h1>Trello 2</h1>
+    <div>
+      {getData.map((data, _) => <h1 key={data.id}>{data.name}</h1>)}
+    </div>
   )
 }
 
